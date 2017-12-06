73c800561f7364eebb96f3246fad7a423817ca49
suiffet guillaume <suiffet.g@gmail.com>
2017-12-06 03:18:23 +0100

    feature clic button contact call agiloffice & install request

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 var express = require('express');
 var app = express();
+var request = require('request');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.static('public/css'));
 
 app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.post('/call', function (req, res) {
+    request({
+        headers: {
+            //'agil-cookie': 'session_id=be423de133d16b55fd6da144ec31d222',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json'
+        },
+        url: 'https://ws.agiloffice.fr/demo/session_login?username=demo3&password=demo3',
+        method: 'POST'
+    }, function (err, res, body) {
+        console.log("body", body);
+        console.log("err", res);
+        console.log('statusCode:', res && res.statusCode);
+    });
     res.render('index');
 });
 
