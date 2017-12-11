1b2c46120b58b7f194d95f43f3cab0bb9f022dff
suiffet guillaume <suiffet.g@gmail.com>
2017-12-11 11:17:16 +0100

    authentification via form get session login

--- server.js
+++ server.js
@@ -1,40 +1,46 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+var bodyParser = require('body-parser');
 var cookie = "";
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.static('public/css'));
 
-function getCookies(callback){
-    request({
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/json'
-        },
-        url: 'https://ws.agiloffice.fr/demo/session_login?username=demo3&password=demo3',
-        method: 'POST'
-    }, function (err, res) {
-        console.log('statusCode:', res && res.statusCode);
-        if (!err && res.statusCode == 200) {
-            return callback(null, res.headers['agil-cookie']);
-        } else {
-            return callback(err);
-        }
-    });
-}
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/', function (req, res) {
+    res.render('index', {cookie:cookie});
+});
+
+app.post('/auth', function (req, res) {
+    function getCookies(callback){
+        request({
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Accept': 'application/json'
+            },
+            url: 'https://ws.agiloffice.fr/demo/session_login?username='+req.body.username+'&password='+req.body.password,
+            method: 'POST'
+        }, function (err, res) {
+            console.log('statusCode:', res && res.statusCode);
+            if (!err && res.statusCode == 200) {
+                return callback(null, res.headers['agil-cookie']);
+            } else {
+                return callback(err);
+            }
+        });
+    }
     getCookies(function(err, cookies) {
         if (!err) {
             console.log("cookie" ,"_agil_ws_"+cookies);
             cookie = "_agil_ws_"+cookies;
         }
-        res.setHeader("Set-Cookie",cookie);
-        //res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Credentials','true');
-        res.render('index');
+        res.render('index',{cookie:cookie});
     });
 });
 
