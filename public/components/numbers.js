a23c1b0de0a027ee53096ba37e2ab2b8e89b1a21
suiffet guillaume <suiffet.g@gmail.com>
2017-12-20 16:33:49 +0100

    install socketIo & notification post 8003

--- public/components/numbers.js
+++ public/components/numbers.js
@@ -3,8 +3,12 @@ var React = require("react");
 class Numbers extends React.Component {
     constructor() {
         super();
+        this.state = {
+            post: ""
+        };
     }
     handleClick(number) {
+        var postNumber = this;
         console.log('number', number);
         fetch('https://ws.agiloffice.fr/demo/phone_call?number='+number, {
             method: "POST",
@@ -13,18 +17,30 @@ class Numbers extends React.Component {
             },
             credentials: 'include',
         }).then(function(response) {
+            var socket = io.connect();
             if (response.status == 200) {
-                console.log("response", response);
+                socket.emit('subscribe', '8003');
+                //console.log("socket", socket);
+                socket.on ('messageSuccess', function (post) {
+                    console.log("call", post);
+                    postNumber.setState({
+                        post: post,
+                    });
+                });
             }
         });
     }
     render() {
+        var alert = "";
+        if (this.state.post == "8003" ) {
+            alert = <i className="fa fa-bell" aria-hidden="true"></i>;
+        }
         return (
             <div className="container">
                 <div className="row">
                     <div className="list-group text-center col-xs-12 col-sm-6 col-md-3 col-lg-2">
                         <a href="#" onClick={this.handleClick.bind(this, 8002)} className="list-group-item list-group-item-action">8002</a>
-                        <a href="#" onClick={this.handleClick.bind(this, 8003)} className="list-group-item list-group-item-action">8003</a>
+                        <a href="#" onClick={this.handleClick.bind(this, 8003)} className="list-group-item list-group-item-action">8003 {alert}</a>
                     </div>
                 </div>
             </div>
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
+var http = require('http');
 var express = require('express');
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.static('public/css'));
 
 app.get('/', function (req, res) {
+    io.sockets.on('connection', function (socket) {
+        socket.on('subscribe', function(post) {
+            socket.join(post);
+            //console.log('joining post', post);
+            io.sockets.emit ('messageSuccess', post);
+        })
+    });
     res.render('index');
 });
 
 var port= (process.env.PORT || 8080);
-app.listen(port, function () {
+server.listen(port, function () {
     console.log("Server listening on port 8080");
 });
